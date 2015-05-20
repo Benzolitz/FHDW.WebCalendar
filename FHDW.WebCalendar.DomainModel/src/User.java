783c3a14b0e149c7aad6c49bcc57b713d763e65a
Eduard <Eduard@Eduard-PC>
2015-05-20 13:23:33 +0200

    Created DomainModels User and Event.

--- FHDW.WebCalendar.DomainModel/src/User.java
+++ FHDW.WebCalendar.DomainModel/src/User.java
@@ -1,5 +1,82 @@
 
 public class User
 {
+	private String Username;
+	private String EMail;
+	private String FirstName;
+	private String LastName;
+	private String PhoneNumber;
+	private String SecurityQuestion;
+	private String SecurityAnswer;
+	
+	public void SetUsername(String username)
+	{
+		Username = username;
+	}
+	
+	public String GetUsername()
+	{
+		return Username;
+	}
+	
+	public void SetEMail(String email)
+	{
+		EMail = email;
+	}
+	
+	public String GetEMail()
+	{
+		return EMail;
+	}
+	
+	public void SetFirstName(String firstName)
+	{
+		FirstName = firstName;
+	}
+	
+	public String GetFirstName()
+	{
+		return FirstName;
+	}
+	
+	public void SetLastName(String lastName)
+	{
+		LastName = lastName;
+	}
+	
+	public String GetLastName()
+	{
+		return LastName;
+	}
+	
+	public void SetPhoneNumber(String phoneNumber)
+	{
+		PhoneNumber = phoneNumber;
+	}
+	
+	public String GetPhoneNumber()
+	{
+		return PhoneNumber;
+	}
+	
+	public void SetSecurityQuestion(String securityQuestion)
+	{
+		SecurityQuestion = securityQuestion;
+	}
+	
+	public String GetSecurityQuestion()
+	{
+		return SecurityQuestion;
+	}
+
+	public void SetSecurityAnswer(String securityAnswer)
+	{
+		SecurityAnswer = securityAnswer;
+	}
+	
+	public String GetSecurityAnswer()
+	{
+		return SecurityAnswer;
+	}
 	
 }
