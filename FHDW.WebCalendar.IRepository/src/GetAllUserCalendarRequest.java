df9f307b5c644936f58bb5bfe093564b2509c7f5
Eduard <Eduard@Eduard-PC>
2015-05-25 20:21:18 +0200

    Fixed TestConnection errors.

--- FHDW.WebCalendar.IRepository/src/GetAllUserCalendarRequest.java
+++ FHDW.WebCalendar.IRepository/src/GetAllUserCalendarRequest.java
@@ -3,6 +3,11 @@ public class GetAllUserCalendarRequest extends IRequest
 {
 	private int userId;
 	
+	public GetAllUserCalendarRequest()
+	{
+		
+	}
+	
 	public GetAllUserCalendarRequest(int p_userId)
 	{
 		userId = p_userId;
--- FHDW.WebCalendar.Repository/src/TestConnection.java
+++ FHDW.WebCalendar.Repository/src/TestConnection.java
@@ -5,125 +5,86 @@ import jdk.nashorn.internal.runtime.RewriteException;
 
 import com.mysql.fabric.xmlrpc.base.Array;
 
-
 public class TestConnection
 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
+
 		WebCalendarRepo testRepo = new WebCalendarRepo();
-		
-		//TestCheckUsernameOrEmailWithUsername(testRepo);				//Working
-		//TestCheckUsernameOrEmailWithEmail(testRepo);					//Working
-		//TestCheckUsernameOrEmailWithWrongUsername(testRepo);			//Working
-		
-		//TestValidateLoginWithUsername(testRepo);						//Working
-		//TestValidateLoginWithEmail(testRepo);							//Working
-		//TestValidateLoginWithWrongUsername(testRepo);					//Working
-		//TestValidateLoginWithWrongPassword(testRepo);					//Working
-		
-		//TestRegistrateNewUser(testRepo);								//Working
-		
-		//TestGetSecurityQuestionWithUsername(testRepo);				//Working
-		//TestGetSecurityQuestionWithEmail(testRepo);					//Working
-		//TestGetSecurityQuestionWithWrongUsername(testRepo);			//Working
-		
-		//TestValidateSecurityAnswerCorrectAnswer(testRepo);			//Working
-		//TestValidateSecurityAnswerWrongAnswer(testRepo);				//Working
-		
-		//TestResetPassword(testRepo);									//Working
-		
-		//TestGetEventsForUser(testRepo);								//Working
-		
-		//TestGetEventDetailed(testRepo);								//Working
-		
-		//TestGetAllUserCalendar(testRepo);								//Working
-		
-		//TestSaveEvent(testRepo);
-		
+
+		// TestCheckUsernameOrEmailWithUsername(testRepo); //Working
+		// TestCheckUsernameOrEmailWithEmail(testRepo); //Working
+		// TestCheckUsernameOrEmailWithWrongUsername(testRepo); //Working
+
+		// TestValidateLoginWithUsername(testRepo); //Working
+		// TestValidateLoginWithEmail(testRepo); //Working
+		// TestValidateLoginWithWrongUsername(testRepo); //Working
+		// TestValidateLoginWithWrongPassword(testRepo); //Working
+
+		// TestRegistrateNewUser(testRepo); //Working
+
+		// TestGetSecurityQuestionWithUsername(testRepo); //Working
+		// TestGetSecurityQuestionWithEmail(testRepo); //Working
+		// TestGetSecurityQuestionWithWrongUsername(testRepo); //Working
+
+		// TestValidateSecurityAnswerCorrectAnswer(testRepo); //Working
+		// TestValidateSecurityAnswerWrongAnswer(testRepo); //Working
+
+		// TestResetPassword(testRepo); //Working
+
+		// TestGetEventsForUser(testRepo); //Working
+
+		// TestGetEventDetailed(testRepo); //Working
+
+		// TestGetAllUserCalendar(testRepo); //Working
+
+		// TestSaveEvent(testRepo);
+
 	}
-	
-	private static void TestCheckUsernameOrEmailWithUsername(WebCalendarRepo testRepo)
+
+	private static void TestCheckUsernameOrEmailWithUsername(
+			WebCalendarRepo testRepo)
 	{
 		GetUserIdResponse response;
 		GetUserIdRequest request = new GetUserIdRequest();
 		request.SetUsernameOrEmail("User1");
-		
+
 		response = testRepo.GetUserId(request);
-		System.out.println("TestCheckUsernameOrEmailWithUsername was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out
+				.println("TestCheckUsernameOrEmailWithUsername was successfully: "
+						+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
 	}
-	
-	private static void TestCheckUsernameOrEmailWithEmail(WebCalendarRepo testRepo)
+
+	private static void TestCheckUsernameOrEmailWithEmail(
+			WebCalendarRepo testRepo)
 	{
 		GetUserIdResponse response;
 		GetUserIdRequest request = new GetUserIdRequest();
 		request.SetUsernameOrEmail("Email1@Mail.de");
-		
+
 		response = testRepo.GetUserId(request);
-		System.out.println("TestCheckUsernameOrEmailWithEmail was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out
+				.println("TestCheckUsernameOrEmailWithEmail was successfully: "
+						+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
 	}
-	
-	private static void TestCheckUsernameOrEmailWithWrongUsername(WebCalendarRepo testRepo)
+
+	private static void TestCheckUsernameOrEmailWithWrongUsername(
+			WebCalendarRepo testRepo)
 	{
 		GetUserIdResponse response;
 		GetUserIdRequest request = new GetUserIdRequest();
 		request.SetUsernameOrEmail("UserWhichDoesntExist");
-		
+
 		response = testRepo.GetUserId(request);
-		System.out.println("TestCheckUsernameOrEmailWithUsername was successfully: " + String.valueOf(!response.IsSuccess()));
+		System.out
+				.println("TestCheckUsernameOrEmailWithUsername was successfully: "
+						+ String.valueOf(!response.IsSuccess()));
 		System.out.println(response.GetMessage());
 	}
-	
-	private static void TestValidateLoginWithUsername(WebCalendarRepo testRepo)
-	{
-		ValidateLoginResponse response;
-		ValidateLoginRequest request = new ValidateLoginRequest();
-		request.SetUsernameOrEmail("User1");
-		request.SetPassword("pass1");
-		
-		response = testRepo.ValidateLogin(request);
-		System.out.println("TestValidateLoginWithUsername was successfully: " + String.valueOf(response.IsSuccess()));
-		System.out.println(response.GetMessage());
-	}
-	
-	private static void TestValidateLoginWithEmail(WebCalendarRepo testRepo)
-	{
-		ValidateLoginResponse response;
-		ValidateLoginRequest request = new ValidateLoginRequest();
-		request.SetUsernameOrEmail("Email1@Mail.de");
-		request.SetPassword("pass1");
-		
-		response = testRepo.ValidateLogin(request);
-		System.out.println("TestValidateLoginWithEmail was successfully: " + String.valueOf(response.IsSuccess()));
-		System.out.println(response.GetMessage());
-	}
-	
-	private static void TestValidateLoginWithWrongUsername(WebCalendarRepo testRepo)
-	{
-		ValidateLoginResponse response;
-		ValidateLoginRequest request = new ValidateLoginRequest();
-		request.SetUsernameOrEmail("UserWhichDoesntExist");
-		request.SetPassword("pass1");
-		
-		response = testRepo.ValidateLogin(request);
-		System.out.println("TestValidateLoginWithWrongUsername was successfully: " + String.valueOf(!response.IsSuccess()));
-		System.out.println(response.GetMessage());
-	}
-	
-	private static void TestValidateLoginWithWrongPassword(WebCalendarRepo testRepo)
-	{
-		ValidateLoginResponse response;
-		ValidateLoginRequest request = new ValidateLoginRequest();
-		request.SetUsernameOrEmail("User1");
-		request.SetPassword("WrongPass");
-		
-		response = testRepo.ValidateLogin(request);
-		System.out.println("TestValidateLoginWithWrongPassword was successfully: " + String.valueOf(!response.IsSuccess()));
-		System.out.println(response.GetMessage());
-	}
-	
+
 	private static void TestRegistrateNewUser(WebCalendarRepo testRepo)
 	{
 		RegistrateNewUserResponse response;
@@ -136,123 +97,116 @@ public class TestConnection
 		request.SetPhoneNumber("+49 1234/56789");
 		request.SetSecurityQuestion(1);
 		request.SetSecurityAnswer("Geheime Antwort");
-		
+
 		response = testRepo.RegistrateNewUser(request);
-		System.out.println("TestRegistrateNewUser was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out.println("TestRegistrateNewUser was successfully: "
+				+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
 	}
-	
-	private static void TestGetSecurityQuestionWithUsername(WebCalendarRepo testRepo)
+
+	private static void TestGetSecurityQuestionWithUsername(
+			WebCalendarRepo testRepo)
 	{
 		GetSecurityQuestionResponse response;
 		GetSecurityQuestionRequest request = new GetSecurityQuestionRequest();
 		request.SetUsernameOrEmail("User1");
-		
+
 		response = testRepo.GetSecurityQuestion(request);
-		System.out.println("TestGetSecurityQuestionWithUsername was successfully: " + String.valueOf(response.IsSuccess()));
-		System.out.println(response.GetMessage() + " " + response.GetSecurityQuestion());
+		System.out
+				.println("TestGetSecurityQuestionWithUsername was successfully: "
+						+ String.valueOf(response.IsSuccess()));
+		System.out.println(response.GetMessage() + " "
+				+ response.GetSecurityQuestion());
 	}
-	
-	private static void TestGetSecurityQuestionWithEmail(WebCalendarRepo testRepo)
+
+	private static void TestGetSecurityQuestionWithEmail(
+			WebCalendarRepo testRepo)
 	{
 		GetSecurityQuestionResponse response;
 		GetSecurityQuestionRequest request = new GetSecurityQuestionRequest();
 		request.SetUsernameOrEmail("Email1@Mail.de");
-		
+
 		response = testRepo.GetSecurityQuestion(request);
-		System.out.println("TestGetSecurityQuestionWithEmail was successfully: " + String.valueOf(response.IsSuccess()));
-		System.out.println(response.GetMessage() + " " + response.GetSecurityQuestion());
+		System.out
+				.println("TestGetSecurityQuestionWithEmail was successfully: "
+						+ String.valueOf(response.IsSuccess()));
+		System.out.println(response.GetMessage() + " "
+				+ response.GetSecurityQuestion());
 	}
-	
-	private static void TestGetSecurityQuestionWithWrongUsername(WebCalendarRepo testRepo)
+
+	private static void TestGetSecurityQuestionWithWrongUsername(
+			WebCalendarRepo testRepo)
 	{
 		GetSecurityQuestionResponse response;
 		GetSecurityQuestionRequest request = new GetSecurityQuestionRequest();
 		request.SetUsernameOrEmail("UserWhichNotExists");
-		
+
 		response = testRepo.GetSecurityQuestion(request);
-		System.out.println("TestGetSecurityQuestionWithWrongUsername was successfully: " + String.valueOf(!response.IsSuccess()));
-		System.out.println(response.GetMessage() + " " + response.GetSecurityQuestion());
+		System.out
+				.println("TestGetSecurityQuestionWithWrongUsername was successfully: "
+						+ String.valueOf(!response.IsSuccess()));
+		System.out.println(response.GetMessage() + " "
+				+ response.GetSecurityQuestion());
 	}
-	
-	private static void TestValidateSecurityAnswerCorrectAnswer(WebCalendarRepo testRepo)
-	{
-		ValidateSecurityAnswerResponse response;
-		ValidateSecurityAnswerRequest request = new ValidateSecurityAnswerRequest();
-		request.SetUsernameOrEmail("User1");
-		request.SetAnswer("Hund");
-		
-		response = testRepo.ValidateSecurityAnswer(request);
-		System.out.println("TestValidateSecurityAnswerCorrectAnswer was successfully: " + String.valueOf(response.IsSuccess()));
-		System.out.println(response.GetMessage());
-	}
-	
-	private static void TestValidateSecurityAnswerWrongAnswer(WebCalendarRepo testRepo)
-	{
-		ValidateSecurityAnswerResponse response;
-		ValidateSecurityAnswerRequest request = new ValidateSecurityAnswerRequest();
-		request.SetUsernameOrEmail("User1");
-		request.SetAnswer("Katze");
-		
-		response = testRepo.ValidateSecurityAnswer(request);
-		System.out.println("TestValidateSecurityAnswerWrongAnswer was successfully: " + String.valueOf(!response.IsSuccess()));
-		System.out.println(response.GetMessage());
-	}
-	
+
 	private static void TestResetPassword(WebCalendarRepo testRepo)
 	{
 		ResetPasswordResponse response;
 		ResetPasswordRequest request = new ResetPasswordRequest();
 		request.SetUsernameOrEmail("User1");
 		request.SetPassword("newPass1");
-		
+
 		response = testRepo.ResetPassword(request);
-		System.out.println("TestResetPassword was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out.println("TestResetPassword was successfully: "
+				+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
 	}
-	
+
 	private static void TestGetEventsForUser(WebCalendarRepo testRepo)
 	{
 		GetEventsForUserResponse response;
 		GetEventsForUserRequest request = new GetEventsForUserRequest();
 		request.SetCalendarId(1);
 		request.SetUserId(1);
-		
+
 		response = testRepo.GetEventsForUser(request);
-		System.out.println("TestGetEventsForUser was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out.println("TestGetEventsForUser was successfully: "
+				+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
-		if(response.IsSuccess())
+		if (response.IsSuccess())
 			for (EventCalendarView event : response.GetEvents())
 				System.out.println(event.GetTitle());
 	}
-	
+
 	private static void TestGetEventDetailed(WebCalendarRepo testRepo)
 	{
 		GetEventDetailedResponse response;
 		GetEventDetailedRequest request = new GetEventDetailedRequest();
 		request.SetEventId(1);
-		
+
 		response = testRepo.GetEventDetailed(request);
-		System.out.println("TestGetEventDetailed was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out.println("TestGetEventDetailed was successfully: "
+				+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
-		if(response.IsSuccess())
+		if (response.IsSuccess())
 		{
 			System.out.println("Title:" + response.GetEvent().GetTitle());
 			for (String s : response.GetEvent().GetRequiredUser())
 				System.out.println("Required User:" + s);
 		}
 	}
-	
+
 	private static void TestGetAllUserCalendar(WebCalendarRepo testRepo)
 	{
 		GetAllUserCalendarResponse response;
 		GetAllUserCalendarRequest request = new GetAllUserCalendarRequest();
 		request.SetUserId(1);
-		
+
 		response = testRepo.GetAllUserCalendar(request);
-		System.out.println("GetAllUserCalendar was successfully: " + String.valueOf(response.IsSuccess()));
+		System.out.println("GetAllUserCalendar was successfully: "
+				+ String.valueOf(response.IsSuccess()));
 		System.out.println(response.GetMessage());
-		if(response.IsSuccess())
+		if (response.IsSuccess())
 		{
 			for (Calendar calendar : response.getCalendars())
 			{
@@ -260,24 +214,5 @@ public class TestConnection
 			}
 		}
 	}
-	
-	private static void TestSaveEvent(WebCalendarRepo testRepo)
-	{
-		User testUser = new User();
-		testUser.SetId(1);
-		
-		Event testEvent = new Event();
-		testEvent.SetCategory(new ArrayList<String>(){{add("5");}});
-		testEvent.SetCreationTime(new Date());
-		testEvent.SetCreator(testUser);
-		testEvent.SetEndTime(new Date());
-		testEvent.SetLocation("Location Troll");
-		testEvent.SetMessage("Message Troll");
-		testEvent.SetStartTime(new Date());
-		
-		testRepo.SaveEvent(testEvent);
-	}
 
-
-	
 }
