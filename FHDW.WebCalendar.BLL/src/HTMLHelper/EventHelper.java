bc1d23c14ae6fb29a372451fab26c6ccd6aa001a
Freddi <Frederik@Heinrichs-online.de>
2015-06-07 16:59:33 +0200

    Datumsformatierung in BLL verlagert

--- FHDW.WebCalendar.BLL/src/HTMLHelper/EventHelper.java
+++ FHDW.WebCalendar.BLL/src/HTMLHelper/EventHelper.java
@@ -1,5 +1,7 @@
 package HTMLHelper;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import Exceptions.IOException;
@@ -14,6 +16,16 @@ import Model.Calendar.Event.Event;
  */
 public class EventHelper
 {	
+	
+	private static SimpleDateFormat dateFormatter;
+	
+	public static SimpleDateFormat GetDateFormatter() {
+		if (EventHelper.dateFormatter == null) {
+			EventHelper.dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		}
+		return EventHelper.dateFormatter;
+	}
+	
 	public static void checkEventData(Event event) throws IOException { 
 		//throw new IOException("");	
 	}
@@ -24,6 +36,11 @@ public class EventHelper
 	
 	public static void checkDateTime(Date p_dateTime) throws IOException {
 		//throw new IOException("");	
+	}
+	
+	public static String formatDateTime(Calendar p_calendar) {
+
+		return GetDateFormatter().format(p_calendar.getTime());
 	}
 
 }
