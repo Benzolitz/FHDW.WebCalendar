a796724ea6e775c6be4eae5d186fabe58921f690
Freddi <Frederik@Heinrichs-online.de>
2015-06-07 16:59:55 +0200

    SearchService an neue EventKlasen angepasst

--- FHDW.WebCalendar.BLL/src/Services/SearchService.java
+++ FHDW.WebCalendar.BLL/src/Services/SearchService.java
@@ -1,7 +1,9 @@
 package Services;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import Exceptions.DatabaseException;
@@ -30,28 +32,32 @@ public class SearchService extends BaseService
 	public List<EventCalendarView> searchEvents(int p_userId, String p_searchString) throws DatabaseException, NotFound {
 		CalenderService calendarService = new CalenderService();
 		List<EventCalendarView> result = new ArrayList<EventCalendarView>();
-		Collection<EventCalendarView> events = calendarService.GetAllEvents(p_userId);
 		
-//		
-//		
-//		boolean found = false;
-//		for(EventCalendarView event : events) {
-//			
-//			if (event.GetTitle().contains(p_searchString)) {
-//				found = true;
-//			} else if(event.GetLocation().contains(p_searchString)) {
-//				found = true;
-//			}
-//			
+		// Setze den SuchZeitram von 1.1.2000 - 31.12.2050
+		Calendar DateFrom = Calendar.getInstance();
+		DateFrom.setTime(new Date(2000, 1, 1));
+		
+		Calendar DateTo = Calendar.getInstance();
+		DateFrom.setTime(new Date(2050, 12, 31));
+		
+		Collection<EventCalendarView> events = calendarService.GetEventsFromTo(p_userId, DateFrom, DateTo);
+		
+		boolean found = false;
+		for(EventCalendarView event : events) {
+			
+			if (event.GetTitle().contains(p_searchString)) {
+				found = true;
+			}
+			
 //			for (String category : event.GetCategory()) {
 //				if (category.contains(p_searchString)) {
 //					found = true;
 //				}
 //			}
-//			if (found) {
-//				result.add(event);
-//			}
-//		}
+			if (found) {
+				result.add(event);
+			}
+		}
 		return result;
 	}
 }
